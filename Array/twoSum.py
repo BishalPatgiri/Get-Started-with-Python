0e639aaf4cbdfb9744da9817f386faae86ec05f0
BishalPatgiri <bishalpatgiri67@gmail.com>
2023-08-13 21:33:45 +0530

    Added twoSum problem advance approach

--- Array/twoSum.py
+++ Array/twoSum.py
@@ -6,7 +6,7 @@ index=0
 target=10
 inputArray=[2,3,5,8,9]
 
-
+## Brute force approach
 def findTwoSum():
     global index
     global output
@@ -15,7 +15,23 @@ def findTwoSum():
        for j in range(i+1,len(inputArray)):
             if((inputArray[i]+inputArray[j])==target):
                 output.append(i)
-                output.append(j)
-                break
+                output.append(j) # output=[i,j]
+                break # return
 findTwoSum()
-print(output)
+print("Brute force output:",output)
+
+## Hash Map approach
+
+def findTwoSum():
+    global index
+    hashMap={}
+    for i in range(0,len(inputArray)):
+      reminder=target-inputArray[i]
+      if reminder in hashMap:
+        return [hashMap[reminder],i]
+      else:
+        hashMap[reminder]=i
+
+
+findTwoSum()
+print("Hash Map output:",output)
